8e3cd3c2ac6c4b25ddf4e8f404921502965aef59
Hernan Mathias Farina Forster <hernan.farinaforster@dawsoncollege.qc.ca>
2023-08-10 04:07:37 -0400

    delist/enroll for student users finished

--- website/enrollCourse.php
+++ website/enrollCourse.php
@@ -1,11 +1,12 @@
 <?php
     include 'navbar.php';
     try {
-        if (isset($_POST['course_id'])) {
+        if (isset($_POST['course_id']) && isset($_POST['course_conclusion'])) {
 
             $user_id = $_SESSION['id'];
             $funds = $_SESSION['funds'];
             $course_id = (int)$_POST['course_id'];
+            $optionSelected = $_POST['course_conclusion'];
     
             $sql = 'SELECT * FROM Course WHERE id = :course_id';
             $stmtCourse = $pdo->prepare($sql); 
@@ -16,27 +17,53 @@
                 $course = $stmtCourse->fetch(PDO::FETCH_ASSOC);
                 $id_instructor = $course['id_instructor'];
                 $cost = $course['cost'];
+
+                if($optionSelected === 'EN') {
+                    if ($cost <= $funds) {
+                        $sqlInsert = "INSERT INTO inscription (id_student, id_course, id_instructor) VALUES (:id_student, :id_course, :id_instructor)";
+                        $stmtInsert = $pdo->prepare($sqlInsert); 
+                        $stmtInsert->execute(array(
+                            ':id_student' => $user_id,
+                            ':id_course' => $course_id,
+                            ':id_instructor' => $id_instructor));
     
-                if ($cost <= $funds) {
-                    $sqlInsert = "INSERT INTO inscription (id_student, id_course, id_instructor) VALUES (:id_student, :id_course, :id_instructor)";
-                    $stmtInsert = $pdo->prepare($sqlInsert); 
-                    $stmtInsert->execute(array(
-                        ':id_student' => $user_id,
-                        ':id_course' => $course_id,
-                        ':id_instructor' => $id_instructor));
-
-                    $sql = "UPDATE Student SET funds = :funds - :cost WHERE id = :id";
-                    $stmt = $pdo->prepare($sql);
-                    $stmt->execute([':funds' => $funds, ':cost' => $cost, 'id' => $user_id]);
-
-                    $sqlUpdate = "UPDATE Student SET funds = :new_funds WHERE id = :id";
-                    $new_funds = $funds - $cost;
-                    $stmtUpdate = $pdo->prepare($sqlUpdate);
-                    $stmtUpdate->execute([':new_funds' => $new_funds, 'id' => $user_id]);
-
-                    $_SESSION['funds'] = $new_funds;
-                } else {
-                    $error_message = "You do not have enough funds to buy this course.";
+                        $sql = "UPDATE Student SET funds = :funds - :cost WHERE id = :id";
+                        $stmt = $pdo->prepare($sql);
+                        $stmt->execute([':funds' => $funds, ':cost' => $cost, 'id' => $user_id]);
+    
+                        $sqlUpdate = "UPDATE Student SET funds = :new_funds WHERE id = :id";
+                        $new_funds = $funds - $cost;
+                        $stmtUpdate = $pdo->prepare($sqlUpdate);
+                        $stmtUpdate->execute([':new_funds' => $new_funds, 'id' => $user_id]);
+    
+                        $_SESSION['funds'] = $new_funds;
+                    } else {
+                        $error_message = "You do not have enough funds to buy this course.";
+                    }
+                } elseif ($optionSelected === 'DE') {
+                    $sqlSearch = "SELECT * FROM inscription WHERE id_student = :id_student AND id_course = :id_course";
+                    $stmtInscription = $pdo->prepare($sqlSearch);
+                    $stmtInscription->bindParam(':id_student', $user_id, PDO::PARAM_INT);
+                    $stmtInscription->bindParam(':id_course', $course_id, PDO::PARAM_INT);
+                    $stmtInscription->execute();
+                
+                    if ($stmtInscription->rowCount() > 0) { 
+                        $sqlDelete = "DELETE FROM inscription WHERE id_student = :id_student AND id_course = :id_course";
+                        $stmtDelete = $pdo->prepare($sqlDelete);
+                        $stmtDelete->execute(array(
+                            ':id_course' => $course_id,
+                            ':id_student' => $user_id
+                        ));
+                    
+                        $sqlUpdate = "UPDATE student SET funds = funds + :refund WHERE id = :id_student";
+                        $stmtUpdate = $pdo->prepare($sqlUpdate);
+                        $stmtUpdate->execute(array(
+                            ':refund' => $cost,
+                            ':id_student' => $user_id
+                        ));
+                    } else {
+                        $error_message = "You are not in this course";
+                    }
                 }
             } else {
                 $error_message = "Course ID not found.";
@@ -48,7 +75,7 @@
             exit();
         }
 ?>
-<title>Course Conlusion</title>
+<title>Course Conclusion</title>
 <?php if (!empty($error_message)) : ?>
     <div id="alert-border-2" class="flex items-center p-4 text-red-800 border-t-4 border-red-300 bg-red-50 dark:text-red-400 dark:bg-gray-800 dark:border-red-800" role="alert">
         <svg class="flex-shrink-0 w-4 h-4" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 20">
@@ -79,8 +106,8 @@
                 <label for="course_conclusion" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Course action:</label>
                     <select id="course_conclusion" name="course_conclusion" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
                         <option selected>Choose one option</option>
-                        <option value="SC">Enroll</option>
-                        <option value="MT">Delist</option>
+                        <option value="EN">Enroll</option>
+                        <option value="DE">Delist</option>
                     </select>
             </div>
             <button type="submit" class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Delist/Enroll</button>
