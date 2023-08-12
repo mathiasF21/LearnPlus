1f2bdbd7eacc69cc5653137fd60ee7acc43d36ea
Hernan Mathias Farina Forster <hernan.farinaforster@dawsoncollege.qc.ca>
2023-08-12 00:54:23 -0400

    edit profile fully completed, added a functionality to add funds to the student's account.

--- website/editProfile.php
+++ website/editProfile.php
@@ -1,14 +1,24 @@
 <?php include 'navbar.php';
     try {
         if($_SERVER['REQUEST_METHOD'] === 'POST') {
-            if (isset($_POST['first_name']) && isset($_POST['last_name'])) {
+            if((int)$_POST['st_funds'] < 0) {
+                $error_message = "The amount of funds being added cannot be negative.";
+            } elseif (isset($_POST['first_name']) && isset($_POST['last_name']) && isset($_POST['st_funds'])) {
                 $email = $_SESSION['email'];
                 $last_name = $_POST['last_name'];
                 $first_name = $_POST['first_name'];
-            
+                $new_funds = $_POST['st_funds'];
+                $funds = $_SESSION['funds'];
+
                 $sql = "UPDATE users SET first_name = :first_name, last_name = :last_name WHERE email = :email";
                 $stmt = $pdo->prepare($sql);
                 $stmt->execute(['first_name' => $first_name, 'last_name' => $last_name, 'email' => $email]);
+
+                $new_funds_total = $funds + $new_funds; 
+
+                $sqlUpdate = "UPDATE Student SET funds = :new_funds WHERE id = :id";
+                $stmtUpdate = $pdo->prepare($sqlUpdate);
+                $stmtUpdate->execute([':new_funds' => $new_funds_total, 'id' => $_SESSION['id']]);
 
                 $_SESSION['first_name'] = $first_name;
                 $_SESSION['last_name'] = $last_name;
@@ -37,6 +47,10 @@
             <div class="mb-6">
                 <label for="last_name" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Last name:</label>
                 <input type="text" id="last_name" name="last_name" value=<?php echo $_SESSION['last_name']; ?> class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Last name" required>
+            </div>
+            <div class="mb-6">
+                <label for="st_funds" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Add funds</label>   
+                <input type="number" id="st_funds" name="st_funds" value=0 class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="First name" required>
             </div>
             <button type="submit" class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Confirm edit</button>
         </form>
